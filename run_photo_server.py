7ca99f55300ada8cbb0d01d1f52c3685d97ef522
Kostiantyn Lavronenko <lavronenkostya@gmail.com>
2025-04-03 00:06:01 +0200

    feat: implement logging

--- run_photo_server.py
+++ run_photo_server.py
@@ -1,19 +1,28 @@
 import threading
 import time 
 import os
+import logging
 
 time_delay = 60*60 # 6
 class ScheduleTask(threading.Thread):
     def __init__(self):
+        
+        logging.basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=logging.INFO,
+            filename = "log.log"
+        )
+
         super(ScheduleTask, self).__init__(name="PhotoServer")
 
 
     def run(self):
         while True:
-            print("Taking an image")
+            logging.info("Taking an image")
             cureent_time = time.strftime("%Y-%m-%d_%Hh%Mm")
             take_image_prompt = f"libcamera-still --width 1536 --height 1024 -o /var/www/html/images/image{cureent_time}.jpg"
-            os.system(take_image_prompt)
+            out = os.popen(take_image_prompt).read()
+            logging.info(out)
             time.sleep(time_delay)
             
 if __name__ == "__main__":
