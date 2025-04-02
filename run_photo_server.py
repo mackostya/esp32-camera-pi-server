245214441dddd823dda88d1a619b83da84564cd3
Kostiantyn Lavronenko <lavronenkostya@gmail.com>
2025-04-02 23:58:31 +0200

    fix: use the correct time delay

--- run_photo_server.py
+++ run_photo_server.py
@@ -14,7 +14,7 @@ class ScheduleTask(threading.Thread):
             cureent_time = time.strftime("%Y-%m-%d_%Hh%Mm")
             take_image_prompt = f"libcamera-still --width 1536 --height 1024 -o /var/www/html/images/image{cureent_time}.jpg"
             os.system(take_image_prompt)
-            time.sleep(10)
+            time.sleep(time_delay)
             
 if __name__ == "__main__":
     task = ScheduleTask()
