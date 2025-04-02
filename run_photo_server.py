f614eaaf1e73cbfbd1461648e4b4f36a731a193c
Kostiantyn Lavronenko <lavronenkostya@gmail.com>
2025-04-03 00:52:26 +0200

    fix: fix the timing zone issue on rpi

--- run_photo_server.py
+++ run_photo_server.py
@@ -2,6 +2,7 @@ import threading
 import time 
 import os
 import logging
+from datetime import datetime
 
 time_delay = 60*60 # 6
 class ScheduleTask(threading.Thread):
@@ -19,12 +20,13 @@ class ScheduleTask(threading.Thread):
     def run(self):
         while True:
             logging.info("Taking an image")
-            cureent_time = time.strftime("%Y-%m-%d_%Hh%Mm")
-            take_image_prompt = f"libcamera-still --width 1536 --height 1024 -o /var/www/html/images/image{cureent_time}.jpg"
+
+            curent_time = f"{datetime.now().year}-{datetime.now().month}-{datetime.now().day}_{datetime.now().hour + 1:02d}:{datetime.now().minute:02d}"
+            take_image_prompt = f"libcamera-still --width 1536 --height 1024 -o /var/www/html/images/image{curent_time}.jpg"
             out = os.popen(take_image_prompt).read()
             logging.info(out)
             time.sleep(time_delay)
-            
+
 if __name__ == "__main__":
     task = ScheduleTask()
     task.start()
